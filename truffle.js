dd72edf7a8e6615e0194a9c673d5a5f458654260
Quinten De Swaef <quintendeswaef@gmail.com>
2017-08-08 17:28:52 +0200

    added truffle config

---
+++ truffle.js
@@ -0,0 +1,51 @@
+fs = require('fs')
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var getMnemonic = function (env) {
+  try {
+    var mnemonic = fs.readFileSync('./config/' + env + '/mnemonic').toString()
+    console.log("using predefined mnemonic for network " + env);
+    return mnemonic;
+  } catch (exception) {
+    return "diplr"
+  }
+}
+
+var getSecret = function () {
+  try {
+    return fs.readFileSync('./config/secrets/infura-token.js').toString();
+  } catch (ex) {
+    console.log(ex);
+    return "";
+  }
+}
+
+module.exports = {
+
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545
+    },
+    local: {
+      provider: new HDWalletProvider(getMnemonic('local'), "http://localhost:8545/"),
+      network_id: '*',
+    },
+    kovan: {
+      network_id: '42',
+      host: "https://kovan.infura.io/" + getSecret(),
+      provider: new HDWalletProvider(getMnemonic('kovan'), "https://kovan.infura.io/" + getSecret())
+    },
+    ropsten: {
+      network_id: '3',
+      host: "https://ropsten.infura.io/",
+      provider: new HDWalletProvider(getMnemonic('ropsten'), "https://ropsten.infura.io/" + getSecret())
+    },
+    rinkeby: {
+      network_id: '4',
+      host: "https://rinkeby.infura.io/" + getSecret(),
+      provider: new HDWalletProvider(getMnemonic('rinkeby'), "https://rinkeby.infura.io/" + getSecret())
+    }
+  },
+};
