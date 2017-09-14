3c62526d9e3235d7804a900f1b1f5cf66db66aa4
Quinten De Swaef <quintendeswaef@gmail.com>
2017-09-14 16:21:36 +0200

    prettyer testing

--- test/DiplrRegistryTest.js
+++ test/DiplrRegistryTest.js
@@ -1,50 +1,48 @@
 const Registry = artifacts.require('./registry/DiplrRegistry.sol');
 const expect = require('chai').expect;
 contract('DiplrRegistry', function (accounts) {
-    console.log(accounts[0]);
-    it('should not be possible to add a context as non-owner', function () {
-        var registry;
-        return Registry.deployed()
-            .then(function (instance) {
-                registry = instance;
-                return registry.setAddress(web3.sha3('registry'), accounts[0], {
-                    from: accounts[1]
-                }).then(function (_) {
-                    console.log(_);
-                    assert.fail('tx should not have succeeded');
-                }).catch(function (err) {
-                    assert.isOk('context wasnt changed', 'which is OK');
 
-                });
-            });
-    });
-    it('should be possible to add a context as owner', function () {
-        var registry;
-        return Registry.deployed().then(function (instance) {
-            registry = instance;
-            return registry.setAddress(web3.sha3('registry2'), accounts[0], {
-                from: accounts[0]
-            });
-        }).then(function (tx) {
-            return registry.getAddress(web3.sha3('registry2'));
-        }).then(function (_address) {
-            assert.equal(_address, accounts[0], "should be updated");
+    let registry;
+
+    beforeEach(async function () {
+        registry = await Registry.new({
+            from: accounts[0]
         });
     });
-    it('should be possible to change a context as owner', function () {
-        var registry;
-        return Registry.deployed().then(function (instance) {
-            registry = instance;
-            return registry.setAddress(web3.sha3('registry3'), accounts[0], {
-                from: accounts[0]
+
+    it('should not be possible to add a context as non-owner', async function () {
+
+        try {
+            await registry.setAddress(web3.sha3('registry'), accounts[0], {
+                from: accounts[1]
             });
-        }).then(function (tx) {
-            registry.setAddress(web3.sha3('registry3'), accounts[1], {
-                from: accounts[0]
-            });
-            return registry.getAddress(web3.sha3('registry3'));
-        }).then(function (_address) {
-            assert.equal(_address, accounts[1], "should be updated");
+            assert.fail('tx should not have succeeded');
+        } catch (error) {
+            assert(
+                error.message.indexOf('invalid opcode') >= 0,
+                'releaseTokenTransfer should throw an opCode exception.'
+            );
+        }
+    });
+
+    it('should be possible to add a context as owner', async function () {
+        await registry.setAddress(web3.sha3('registry2'), accounts[3], {
+            from: accounts[0]
         });
+
+        let _address = await registry.getAddress(web3.sha3('registry2'));
+        assert.equal(_address, accounts[3], "should be updated");
+    });
+    it('should be possible to change a context as owner', async function () {
+
+        await registry.setAddress(web3.sha3('registry3'), accounts[0], {
+            from: accounts[0]
+        });
+        await registry.setAddress(web3.sha3('registry3'), accounts[1], {
+            from: accounts[0]
+        });
+
+        let _address = await registry.getAddress(web3.sha3('registry3'));
+        assert.equal(_address, accounts[1], "should be updated");
     });
 });
